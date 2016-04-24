d69fce8f40706c0773771517210ee94ca9a8481f
vcv <vcv>
2016-04-24 15:52:01 -0500

    First commit.

---
+++ FlightBooking/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace FlightBooking
+{
+	class MainClass
+	{
+
+		private const string DBCONFIG_FILENAME = "dbconn.properties";
+
+		private Query q;
+
+		public MainClass()
+		{
+			/* prepare the database connection stuff */
+			q = new Query();
+			q.OpenConnection();
+		}
+
+		public static void Usage ()
+		{
+			/* prints the choices for commands and parameters */
+			Console.WriteLine ();
+			Console.WriteLine (" *** Please enter one of the following commands *** ");
+			Console.WriteLine ("> login <username> <password>");
+			Console.WriteLine ("> search <origin_city> <destination_city> <direct> <date> <nb itineraries>");
+			Console.WriteLine ("> book <itinerary_id>");
+			Console.WriteLine ("> reservations");
+			Console.WriteLine ("> cancel <reservation_id>");
+			Console.WriteLine ("> quit");
+		}
+
+		public string[] Tokenize(string command)
+		{
+			string pat = "\"([^\"]*)\"|(\\S+)";
+			Regex r = new Regex(pat);
+			// Match the regular expression pattern against a text string.
+			Match m = r.Match(command);
+			List<string> tokens = new List<string> ();
+			while (m.Success) 
+			{
+				for (int i = 1; i <= 2; i++) 
+				{
+					Group g = m.Groups[i];
+					if (g.Length > 0)
+					{
+						tokens.Add (g.Value);
+					}
+				}
+				m = m.NextMatch();
+			}
+			return tokens.ToArray();
+		}
+
+		public void Menu ()
+		{
+			/* prepare to read the user's command and parameter(s) */
+			string command = null;
+
+			while (true) {
+				Usage ();
+				Console.Write ("> ");
+				command = Console.ReadLine ();
+				string[] tokens = Tokenize (command.Trim ());
+				if (tokens.Length == 0) {
+					Console.WriteLine ("Please enter a command");
+					continue; // back to top of loop
+				}
+
+				if (tokens [0].Equals ("login")) {
+					if (tokens.Length == 3) {
+						/* authenticate the user */
+						string username = tokens [1];
+						string password = tokens [2];
+						//q.transaction_login(username, password);
+					} else {
+						Console.WriteLine ("Error: Please provide a username and password");
+					}
+				} else if (tokens [0].Equals ("search")) {
+					/* search for flights */
+					if (tokens.Length == 6) {
+						string originCity = tokens [1];
+						string destinationCity = tokens [2];
+						bool direct = tokens [3].Equals ("1");
+						int day;
+						int count;
+						try {
+							day = Int32.Parse (tokens [4]);
+							count = Int32.Parse (tokens [5]);
+						} catch (FormatException e) {
+							Console.WriteLine ("Failed to parse integer");
+							continue;
+						}
+						Console.WriteLine ("Searching for flights");
+						//q.Transaction_search_unsafe(originCity, destinationCity, direct, day, count);
+						q.Transaction_search_safe(originCity, destinationCity, direct, day, count);
+					} else {
+						Console.WriteLine (
+							"Error: Please provide all search parameters <origin_city> <destination_city> <direct> <date> <nb itineraries>");
+					}
+				} else if (tokens [0].Equals ("book")) {
+					/* book a flight ticket */
+					if (tokens.Length == 2) {
+						int itinerary_id = Int32.Parse (tokens [1]);
+						Console.WriteLine ("Booking itinerary.");
+						//q.transaction_book(itinerary_id);
+					} else {
+						Console.WriteLine ("Error: Please provide an itinerary_id");
+					}
+				} else if (tokens [0].Equals ("reservations")) {
+					/* list all reservations */
+					//q.transaction_reservations();
+				} else if (tokens [0].Equals ("cancel")) {
+					/* cancel a reservation */
+					if (tokens.Length == 2) {
+						int reservation_id = Int32.Parse (tokens [1]);
+						Console.WriteLine ("Canceling reservation.");
+						//q.transaction_cancel(reservation_id);
+					} else {
+						Console.WriteLine ("Error: Please provide a reservation_id");
+					}
+				} else if (tokens [0].Equals ("quit")) {
+					Environment.Exit (0);
+				} else {
+					Console.WriteLine ("Error: unrecognized command '" + tokens [0] + "'");
+				}
+			}
+		}
+
+		public static void Main (string[] args)
+		{
+			MainClass app = new MainClass ();
+			app.Menu ();
+			Console.ReadLine ();
+		}
+
+	}
+}
---
+++ FlightBooking/Query.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using Npgsql;
+
+namespace FlightBooking
+{
+	public class Query
+	{
+
+		private string sqlDriver;
+		private string sqlUrl;
+
+		NpgsqlConnection conn;
+
+		// Logged In User
+		private String username;
+		private int cid; // Unique customer ID
+
+		// Canned queries
+
+		// search (one hop) -- This query ignores the month and year entirely. You
+		// can change it to fix the month and year
+		// to July 2015 or you can add month and year as extra, optional, arguments
+		private string SEARCH_ONE_HOP_SQL = "SELECT year, month_id, day_of_month, carrier_id, flight_num, origin_city, actual_time "
+			+ "FROM Flights " + "WHERE origin_city = :originCity AND dest_city = :destCity AND day_of_month = :dayOfMonth "
+			+ "ORDER BY actual_time ASC LIMIT :limit ";
+
+		public Query ()
+		{
+			sqlDriver = ConfigurationManager.AppSettings ["flightservice.driver"];
+			sqlUrl = ConfigurationManager.AppSettings ["flightservice.url"];
+		}
+
+		public void OpenConnection()
+		{
+			try {
+				conn = new NpgsqlConnection(sqlUrl);
+				conn.Open();
+			}
+			catch(Exception e) {
+				Console.WriteLine (e.Message);
+			}
+		}
+
+		public void CloseConnection()
+		{
+			conn.Close();
+		}
+
+		public void Transaction_login(string username, string password)
+		{
+			// Add code here
+		}
+
+		public void Transaction_search_unsafe (string originCity, string destinationCity, bool directFlight,
+		                                       int dayOfMonth, int numberOfItineraries)
+		{
+			// one hop itineraries
+			string unsafeSearchSQL = "SELECT year, month_id, day_of_month, carrier_id, flight_num, origin_city, actual_time "
+			                         + "FROM Flights "
+			                         + "WHERE origin_city = \'" + originCity + "\' AND dest_city = \'" + destinationCity
+			                         + "\' AND day_of_month =  " + dayOfMonth + " " + "ORDER BY actual_time ASC LIMIT "
+			                         + numberOfItineraries;
+
+			Console.WriteLine ("Submitting query: " + unsafeSearchSQL);
+			try {
+				NpgsqlCommand stmt = new NpgsqlCommand (unsafeSearchSQL, conn);
+				NpgsqlDataReader dr = stmt.ExecuteReader ();
+				while (dr.Read ()) {
+					int result_year = dr.GetInt32 (dr.GetOrdinal ("year"));
+					int result_monthId = dr.GetInt32 (dr.GetOrdinal ("month_id"));
+					int result_dayOfMonth = dr.GetInt32 (dr.GetOrdinal ("day_of_month"));
+					string result_carrierId = dr.GetString (dr.GetOrdinal ("carrier_id"));
+					string result_flightNum = dr.GetString (dr.GetOrdinal ("flight_num"));
+					string result_originCity = dr.GetString (dr.GetOrdinal ("origin_city"));
+					int result_time = dr.GetInt32 (dr.GetOrdinal ("actual_time"));
+					Console.WriteLine ("Flight: " + result_year + "," + result_monthId + "," + result_dayOfMonth + ","
+					+ result_carrierId + "," + result_flightNum + "," + result_originCity + "," + result_time);
+				}
+				dr.Close ();
+			} catch (Exception e) {
+				Console.WriteLine (e.Message);
+			}
+		}
+
+	/**
+	 * Searches for flights from the given origin city to the given destination
+	 * city, on the given day of the month. If "directFlight" is true, it only
+	 * searches for direct flights, otherwise is searches for direct flights and
+	 * flights with two "hops". Only searches for up to the number of
+	 * itineraries given. Prints the results found by the search.
+	 */
+		public void Transaction_search_safe (string originCity, string destCity, bool directFlight,
+											 int dayOfMonth, int numberOfItineraries)
+		{
+			try {
+				// one hop itineraries
+				NpgsqlCommand stmt = new NpgsqlCommand (SEARCH_ONE_HOP_SQL, conn);
+				stmt.Parameters.Add (new NpgsqlParameter ("originCity", NpgsqlTypes.NpgsqlDbType.Varchar));
+				stmt.Parameters.Add (new NpgsqlParameter ("destCity", NpgsqlTypes.NpgsqlDbType.Varchar));
+				stmt.Parameters.Add (new NpgsqlParameter ("dayOfMonth", NpgsqlTypes.NpgsqlDbType.Integer));
+				stmt.Parameters.Add (new NpgsqlParameter ("limit", NpgsqlTypes.NpgsqlDbType.Integer));
+				stmt.Prepare ();
+				stmt.Parameters [0].Value = originCity;
+				stmt.Parameters [1].Value = destCity;
+				stmt.Parameters [2].Value = dayOfMonth;
+				stmt.Parameters [3].Value = numberOfItineraries;
+				NpgsqlDataReader dr = stmt.ExecuteReader ();
+				while (dr.Read ()) {
+					int result_year = dr.GetInt32 (dr.GetOrdinal ("year"));
+					int result_monthId = dr.GetInt32 (dr.GetOrdinal ("month_id"));
+					int result_dayOfMonth = dr.GetInt32 (dr.GetOrdinal ("day_of_month"));
+					string result_carrierId = dr.GetString (dr.GetOrdinal ("carrier_id"));
+					string result_flightNum = dr.GetString (dr.GetOrdinal ("flight_num"));
+					string result_originCity = dr.GetString (dr.GetOrdinal ("origin_city"));
+					int result_time = dr.GetInt32 (dr.GetOrdinal ("actual_time"));
+					Console.WriteLine ("Flight: " + result_year + "," + result_monthId + "," + result_dayOfMonth + ","
+					+ result_carrierId + "," + result_flightNum + "," + result_originCity + "," + result_time);
+				}
+				dr.Close ();
+			} catch (Exception e) {
+				Console.WriteLine (e.Message);
+			}
+			// Add code here
+
+		}
+
+		public void BeginTransaction(string isolationLevel) {
+			try {
+				NpgsqlCommand stmt = new NpgsqlCommand("BEGIN ISOLATION LEVEL SERIALIZABLE;", conn);
+				stmt.ExecuteNonQuery();
+			}
+			catch(Exception e) {
+				Console.WriteLine (e.Message);
+			}
+		}
+
+		public void CommitTransaction() {
+			try {
+				NpgsqlCommand stmt = new NpgsqlCommand("COMMIT;", conn);
+				stmt.ExecuteNonQuery();
+			}
+			catch(Exception e) {
+				Console.WriteLine (e.Message);
+			}
+		}
+
+		public void RollbackTransaction() {
+			try {
+				NpgsqlCommand stmt = new NpgsqlCommand("ROLLBACK;", conn);
+				stmt.ExecuteNonQuery();
+			}
+			catch(Exception e) {
+				Console.WriteLine (e.Message);
+			}
+		}
+
+
+	}
+}
+
